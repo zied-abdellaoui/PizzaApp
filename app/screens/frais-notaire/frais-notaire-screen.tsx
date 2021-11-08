3cdeb9391cc9ef2e24298ac97cb4ce983e65e129
zied-abdellaoui <abdellaoui.zied@hotmail.com>
2021-11-08 18:22:33 +0100

    add the screen to the workflow

---
+++ app/screens/frais-notaire/frais-notaire-screen.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle, TextStyle, View } from "react-native"
+
+import { NavigatorParamList } from "../../navigators"
+import { StackScreenProps } from "@react-navigation/stack"
+import { Screen, Text, Header, GradientBackground } from "../../components"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "../../models"
+import { color, spacing } from "../../theme"
+
+const FULL: ViewStyle = {
+  flex: 1,
+}
+const CONTAINER: ViewStyle = {
+  backgroundColor: color.transparent,
+}
+const HEADER: TextStyle = {
+  paddingBottom: spacing[5] - 1,
+  paddingHorizontal: spacing[4],
+  paddingTop: spacing[3],
+}
+const HEADER_TITLE: TextStyle = {
+  fontSize: 12,
+  fontWeight: "bold",
+  letterSpacing: 1.5,
+  lineHeight: 15,
+  textAlign: "center",
+}
+const LIST_CONTAINER: ViewStyle = {
+  alignItems: "center",
+  flexDirection: "row",
+  padding: 10,
+}
+const IMAGE: ImageStyle = {
+  borderRadius: 35,
+  height: 65,
+  width: 65,
+}
+const LIST_TEXT: TextStyle = {
+  marginLeft: 10,
+}
+const FLAT_LIST: ViewStyle = {
+  paddingHorizontal: spacing[4],
+}
+
+export const FraisNotaireScreen: FC<
+  StackScreenProps<NavigatorParamList, "fraisNotaire">
+> = observer(function FraisNotaireScreen({ navigation }) {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+
+  // Pull in navigation via hook
+  const goBack = () => navigation.goBack()
+  return (
+    <View testID="FraisNotaireScreen" style={FULL}>
+      <GradientBackground colors={["#422443", "#281b34"]} />
+      <Screen style={CONTAINER} preset="fixed" backgroundColor={color.transparent}>
+        <Header
+          headerTx="fraisNotaireScreen.title"
+          leftIcon="back"
+          onLeftPress={goBack}
+          style={HEADER}
+          titleStyle={HEADER_TITLE}
+        />
+        <Text preset="header" text="" />
+      </Screen>
+    </View>
+  )
+})
