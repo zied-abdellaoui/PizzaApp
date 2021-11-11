2628a32e0282b845508655eedbebb1e4f68c5dfa
zied-abdellaoui <abdellaoui.zied@hotmail.com>
2021-11-11 14:28:55 +0100

    working on frais notaire

--- app/components/icon/icons/index.ts
+++ app/components/icon/icons/index.ts
@@ -2,6 +2,7 @@ export const icons = {
   back: require("./arrow-left.png"),
   bullet: require("./bullet.png"),
   bug: require("./ladybug.png"),
+  history: require("./history-solid.png"),
 }
 
 export type IconTypes = keyof typeof icons
--- app/screens/frais-notaire/frais-notaire-screen.tsx
+++ app/screens/frais-notaire/frais-notaire-screen.tsx
@@ -1,14 +1,15 @@
-import React, { FC } from "react"
+import React, { FC, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle, TextStyle, View, TextInputChangeEventData } from "react-native"
 
 import { NavigatorParamList } from "../../navigators"
 import { StackScreenProps } from "@react-navigation/stack"
-import { Screen, Text, Header, GradientBackground, TextField } from "../../components"
+import { Screen, Text, Header, GradientBackground, TextField, Button } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 import { useStores } from "../../models"
 import { color, spacing } from "../../theme"
-import { string } from "mobx-state-tree/dist/internal"
+import Dialog from "react-native-dialog"
+import { translate } from "../../i18n"
 
 const FULL: ViewStyle = {
   flex: 1,
@@ -29,6 +30,20 @@ const HEADER_TITLE: TextStyle = {
   textAlign: "center",
 }
 
+const DEMO: ViewStyle = {
+  paddingVertical: spacing[4],
+  paddingHorizontal: spacing[4],
+  backgroundColor: color.palette.deepPurple,
+}
+const BOLD: TextStyle = { fontWeight: "bold" }
+
+const DEMO_TEXT: TextStyle = {
+  ...BOLD,
+  fontSize: 13,
+  letterSpacing: 2,
+  textTransform: "uppercase",
+}
+
 const INPUT_TEXT: TextStyle = {
   color: color.palette.deepPurple,
 }
@@ -38,6 +53,9 @@ export const FraisNotaireScreen: FC<
 > = observer(function FraisNotaireScreen({ navigation }) {
   // Pull in one of our MST stores
   const { property } = useStores()
+
+  const [saveDialogVisible, setSaveDialogVisible] = useState(false)
+  const [saveSimulationName, setSaveSimulationName] = useState("")
 
   // Pull in navigation via hook
   const goBack = () => navigation.goBack()
@@ -49,6 +67,11 @@ export const FraisNotaireScreen: FC<
     property.setAquisitionCost(isNaN(aquisionCost) ? 0 : aquisionCost)
   }
 
+  const OnSaveSimulationConfirm = () => {
+    console.tron.log("confirme saving...")
+    setSaveDialogVisible(false)
+  }
+
   return (
     <View testID="FraisNotaireScreen" style={FULL}>
       <GradientBackground colors={["#422443", "#281b34"]} />
@@ -56,6 +79,7 @@ export const FraisNotaireScreen: FC<
         <Header
           headerTx="fraisNotaireScreen.title"
           leftIcon="back"
+          rightIcon="history"
           onLeftPress={goBack}
           style={HEADER}
           titleStyle={HEADER_TITLE}
@@ -75,6 +99,37 @@ export const FraisNotaireScreen: FC<
         <Text preset="header" text={property.emolument_4.toLocaleString()} />
         <Text preset="header" text={property.emolument_ht.toLocaleString()} />
         <Text preset="header" text={property.emolument_ttc.toLocaleString()} />
+        <Button
+          labelTx="fraisNotaireScreen.save"
+          style={DEMO}
+          textStyle={DEMO_TEXT}
+          tx="fraisNotaireScreen.save"
+          onPress={() => {
+            const today = new Date()
+            const date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            const dateTime = date + " " + time
+            setSaveSimulationName(dateTime)
+            setSaveDialogVisible(true)
+          }}
+        />
+        <View>
+          <Dialog.Container visible={saveDialogVisible}>
+            <Dialog.Title>{translate("fraisNotaireScreen.saving")}</Dialog.Title>
+            <Dialog.Description>{translate("fraisNotaireScreen.chooseName")}</Dialog.Description>
+            <Dialog.Input value={saveSimulationName} onChangeText={setSaveSimulationName} />
+            <Dialog.Button
+              label={translate("fraisNotaireScreen.cancel")}
+              onPress={() => {
+                setSaveDialogVisible(false)
+              }}
+            />
+            <Dialog.Button
+              label={translate("fraisNotaireScreen.save")}
+              onPress={OnSaveSimulationConfirm}
+            />
+          </Dialog.Container>
+        </View>
       </Screen>
     </View>
   )
